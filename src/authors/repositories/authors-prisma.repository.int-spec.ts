2b87909683eaf316153b3f19fdcb3a3d0cd12510
Cassio <cassiojr0108@gmail.com>
2024-12-01 15:04:05 -0300

    init tests

--- src/authors/repositories/authors-prisma.repository.int-spec.ts
+++ src/authors/repositories/authors-prisma.repository.int-spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundError } from '@/shared/errors/not-found-error'
 import { Test, TestingModule } from '@nestjs/testing'
 import { PrismaClient } from '@prisma/client'
 import { execSync } from 'node:child_process'
@@ -13,11 +14,21 @@ describe('AuthorsPrismaRepository Integration Tests', () => {
     await prisma.$connect()
     module = await Test.createTestingModule({}).compile()
     repository = new AuthorsPrismaRepository(prisma as any)
-  })
+  }, 30000) // Timeout de 30 segundos
 
   beforeEach(async () => {
     await prisma.author.deleteMany()
   })
 
-  afterAll(async () => [await module.close()])
+  afterAll(async () => {
+    await module.close()
+    await prisma.$disconnect()
+  })
+
+  test('should throw an error when the id is not found', async () => {
+    const uuid = 'd628e44c-1ba2-4a39-b52d-2406d1602485'
+    await expect(repository.findById(uuid)).rejects.toThrow(
+      new NotFoundError(`Author not found using ID ${uuid}`),
+    )
+  }, 10000) // Timeout de 10 segundos
 })
