7ce7908ab15299a9375efebdbfd0ba08a0e3d23d
Cassio <cassiojr0108@gmail.com>
2024-12-07 20:21:56 -0300

    Implementing ListAuthorsUsecase int tests

--- src/authors/usecases/list/list-authors.usecase.int-spec.ts
+++ src/authors/usecases/list/list-authors.usecase.int-spec.ts
@@ -1,15 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthorsPrismaRepository } from '../../repositories/authors-prisma.repository'
 import { PrismaClient } from '@prisma/client'
-import { GetAuthor } from './list-author.usecase'
+import { ListAuthor } from './list-authors.usecase'
 import { execSync } from 'node:child_process'
-import { NotFoundError } from '@/shared/errors/not-found-error'
 import { AuthorDataBuilder } from '@/authors/helpers/author-data-builder'
 
-describe('GetAuthor UseCase Integration Tests', () => {
+describe('ListAuthor UseCase Integration Tests', () => {
   let module: TestingModule
   let repository: AuthorsPrismaRepository
-  let usecase: GetAuthor.UseCase
+  let usecase: ListAuthor.UseCase
   const prisma = new PrismaClient()
 
   beforeAll(async () => {
@@ -17,7 +16,7 @@ describe('GetAuthor UseCase Integration Tests', () => {
     await prisma.$connect()
     module = await Test.createTestingModule({}).compile()
     repository = new AuthorsPrismaRepository(prisma as any)
-    usecase = new GetAuthor.UseCase(repository)
+    usecase = new ListAuthor.UseCase(repository)
   })
 
   beforeEach(async () => {
@@ -29,17 +28,75 @@ describe('GetAuthor UseCase Integration Tests', () => {
     await prisma.$disconnect()
   })
 
-  test('should throws an error when the id is not found', async () => {
-    await expect(() =>
-      usecase.execute({ id: 'd628e44c-1ba2-4a39-b52d-2406d1602485' }),
-    ).rejects.toBeInstanceOf(NotFoundError)
+  test('should only apply pagination when the parameters are null', async () => {
+    const createdAt = new Date()
+    const data = []
+    const arrange = Array(3).fill(AuthorDataBuilder({}))
+    arrange.forEach((element, index) => {
+      const timestamp = createdAt.getTime() + index
+      data.push({
+        ...element,
+        email: `author${index}@a.com`,
+        createdAt: new Date(timestamp),
+      })
+    })
+
+    await prisma.author.createMany({ data })
+    const result = await usecase.execute({})
+
+    expect(result).toMatchObject({
+      items: data.reverse(),
+      total: 3,
+      currentPage: 1,
+      perPage: 15,
+      lastPage: 1,
+    })
   })
 
-  test('should be able to get author by id', async () => {
-    const data = AuthorDataBuilder({})
-    const author = await prisma.author.create({ data })
+  test('should apply pagination, filter and ordering', async () => {
+    const createdAt = new Date()
+    const data = []
+    const arrange = ['test', 'a', 'TEST', 'b', 'Test']
+    arrange.forEach((element, index) => {
+      const timestamp = createdAt.getTime() + index
+      data.push({
+        ...AuthorDataBuilder({ name: element }),
+        email: `author${index}@a.com`,
+        createdAt: new Date(timestamp),
+      })
+    })
 
-    const result = await usecase.execute({ id: author.id })
-    expect(result).toStrictEqual(author)
+    await prisma.author.createMany({ data })
+    const result1 = await usecase.execute({
+      page: 1,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'TEST',
+    })
+
+    expect(result1).toMatchObject({
+      items: [data[0], data[4]],
+      total: 3,
+      currentPage: 1,
+      perPage: 2,
+      lastPage: 2,
+    })
+
+    const result2 = await usecase.execute({
+      page: 2,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'TEST',
+    })
+
+    expect(result2).toMatchObject({
+      items: [data[2]],
+      total: 3,
+      currentPage: 2,
+      perPage: 2,
+      lastPage: 2,
+    })
   })
 })
