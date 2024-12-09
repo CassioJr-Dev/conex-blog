e9a0ec34d1b229dbb57accd2035f8aa35db784a5
Cassio <cassiojr0108@gmail.com>
2024-12-09 15:14:59 -0300

    AuthorResolver: implementing createAuthor method

--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -1,14 +1,19 @@
-import { Args, Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { Author } from '../models/author'
 import { Inject } from '@nestjs/common'
 import { ListAuthor } from '@/authors/usecases/list/list-authors.usecase'
 import { SearchParamsArgs } from '../args/search-params.args'
 import { SearchAuthorsResult } from '../models/search-authors-result'
+import { CreateAuthor } from '@/authors/usecases/create/create-author.usecase'
+import { CreateAuthorInput } from '../inputs/create-author.input'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
   @Inject(ListAuthor.UseCase)
   private listAuthorUseCase: ListAuthor.UseCase
+
+  @Inject(CreateAuthor.UseCase)
+  private createAuthorUseCase: CreateAuthor.UseCase
 
   @Query(() => SearchAuthorsResult)
   authors(@Args() { page, perPage, sort, sortDir, filter }: SearchParamsArgs) {
@@ -19,5 +24,10 @@ export class AuthorsResolver {
       sortDir,
       filter,
     })
+  }
+
+  @Mutation(() => Author)
+  createAuthor(@Args('data') data: CreateAuthorInput) {
+    return this.createAuthorUseCase.execute(data)
   }
 }
