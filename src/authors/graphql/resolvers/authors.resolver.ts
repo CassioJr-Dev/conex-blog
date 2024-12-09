ddeef8812666fc1a327b7d22e62910e5847a3ab5
Cassio <cassiojr0108@gmail.com>
2024-12-09 15:28:56 -0300

    AuthorResolver: implementing getAuthorById method

--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -6,6 +6,8 @@ import { SearchParamsArgs } from '../args/search-params.args'
 import { SearchAuthorsResult } from '../models/search-authors-result'
 import { CreateAuthor } from '@/authors/usecases/create/create-author.usecase'
 import { CreateAuthorInput } from '../inputs/create-author.input'
+import { GetAuthor } from '@/authors/usecases/get/get-author.usecase'
+import { AuthorIdArgs } from '../args/author-id.args'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
@@ -15,14 +17,26 @@ export class AuthorsResolver {
   @Inject(CreateAuthor.UseCase)
   private createAuthorUseCase: CreateAuthor.UseCase
 
+  @Inject(GetAuthor.UseCase)
+  private getAuthorUseCase: GetAuthor.UseCase
+
   @Query(() => SearchAuthorsResult)
-  authors(@Args() { page, perPage, sort, sortDir, filter }: SearchParamsArgs) {
+  async authors(
+    @Args() { page, perPage, sort, sortDir, filter }: SearchParamsArgs,
+  ) {
     return this.listAuthorUseCase.execute({
       page,
       perPage,
       sort,
       sortDir,
       filter,
+    })
+  }
+
+  @Query(() => Author)
+  async getAuthorById(@Args() { id }: AuthorIdArgs) {
+    return this.getAuthorUseCase.execute({
+      id,
     })
   }
 
