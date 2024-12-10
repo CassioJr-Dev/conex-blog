93f737cd45a14864ed3755891b8cb20da66634f9
Cassio <cassiojr0108@gmail.com>
2024-12-10 10:37:08 -0300

    Implementing request data validatio

--- src/authors/graphql/args/search-params.args.ts
+++ src/authors/graphql/args/search-params.args.ts
@@ -1,19 +1,32 @@
 import { ArgsType, Field, Int } from '@nestjs/graphql'
+import { IsNumber, IsOptional, IsString, Min } from 'class-validator'
 
 @ArgsType()
 export class SearchParamsArgs {
+  @Min(1)
+  @IsNumber()
+  @IsOptional()
   @Field(() => Int, { nullable: true })
   page?: number
 
+  @Min(1)
+  @IsNumber()
+  @IsOptional()
   @Field(() => Int, { nullable: true })
   perPage?: number
 
+  @IsString()
+  @IsOptional()
   @Field({ nullable: true })
   sort?: string
 
+  @IsString()
+  @IsOptional()
   @Field({ nullable: true })
   sortDir?: 'asc' | 'desc'
 
+  @IsString()
+  @IsOptional()
   @Field({ nullable: true })
   filter?: string
 }
