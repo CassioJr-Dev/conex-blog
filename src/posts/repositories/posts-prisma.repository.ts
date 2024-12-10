27294f23371dab13161bf6ca50c84ad7fb8934a4
Cassio <cassiojr0108@gmail.com>
2024-12-10 14:06:30 -0300

    Implementing findById method

--- src/posts/repositories/posts-prisma.repository.ts
+++ src/posts/repositories/posts-prisma.repository.ts
@@ -1,6 +1,7 @@
 import { PrismaService } from '@/database/prisma/prisma.service'
 import { Post } from '../graphql/models/post'
 import { IPostRepository } from '../interfaces/posts.repository'
+import { NotFoundError } from '@/shared/errors/not-found-error'
 
 export class PostsPrismaRepository implements IPostRepository {
   constructor(private prismaService: PrismaService) {}
@@ -8,16 +9,28 @@ export class PostsPrismaRepository implements IPostRepository {
   create(data: Omit<Post, 'id'>): Promise<Post> {
     throw new Error('Method not implemented.')
   }
+
   update(post: Post): Promise<Post> {
     throw new Error('Method not implemented.')
   }
-  findById(id: string): Promise<Post> {
-    throw new Error('Method not implemented.')
+
+  async findById(id: string): Promise<Post> {
+    return this.get(id)
   }
+
   findBySlug(slug: string): Promise<Post> {
     throw new Error('Method not implemented.')
   }
-  get(id: string): Promise<Post> {
-    throw new Error('Method not implemented.')
+
+  async get(id: string): Promise<Post> {
+    const post = await this.prismaService.post.findUnique({
+      where: { id },
+    })
+
+    if (!post) {
+      throw new NotFoundError(`Post not found using ID ${id}`)
+    }
+
+    return post
   }
 }
