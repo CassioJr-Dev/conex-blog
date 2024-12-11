3078ed5423aa958e7c5074170a2b2c8d69a67a6e
Cassio <cassiojr0108@gmail.com>
2024-12-11 11:34:43 -0300

    Implementing PublishPostUseCase

---
+++ src/posts/usecases/publish/publish-post.usecase.ts
@@ -0,0 +1,24 @@
+import { PostOutput } from '@/posts/dto/post-output'
+import { Post } from '@/posts/graphql/models/post'
+import { PostsPrismaRepository } from '@/posts/repositories/posts-prisma.repository'
+
+export namespace PublishPost {
+  export type Input = {
+    id: string
+  }
+
+  export type Output = PostOutput
+
+  export class UseCase {
+    constructor(private postsRepository: PostsPrismaRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const post = await this.postsRepository.findById(input.id)
+
+      post.published = true
+      const postUpdated = await this.postsRepository.update(post)
+
+      return postUpdated as PostOutput
+    }
+  }
+}
