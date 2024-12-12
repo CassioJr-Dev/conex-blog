572081db7e6b5ebb6413c5cf4be79cee51db1cab
Cassio <cassiojr0108@gmail.com>
2024-12-12 11:14:27 -0300

    PostResolver: implementing publishPost method

--- src/posts/graphql/resolvers/posts.resolver.ts
+++ src/posts/graphql/resolvers/posts.resolver.ts
@@ -13,6 +13,7 @@ import { CreatePostInput } from '../inputs/create-post.input'
 import { GetAuthor } from '@/authors/usecases/get/get-author.usecase'
 import { GetPost } from '@/posts/usecases/get/get-post.usecase'
 import { PostIdArgs } from '../args/post-id.args'
+import { PublishPost } from '@/posts/usecases/publish/publish-post.usecase'
 
 @Resolver(() => Post)
 export class PostsResolver {
@@ -25,6 +26,9 @@ export class PostsResolver {
   @Inject(GetPost.UseCase)
   private getPostUseCase: GetPost.UseCase
 
+  @Inject(PublishPost.UseCase)
+  private publishPostUseCase: PublishPost.UseCase
+
   @Query(() => Post)
   async getPostById(@Args() { id }: PostIdArgs) {
     return this.getPostUseCase.execute({ id })
@@ -33,6 +37,11 @@ export class PostsResolver {
   @Mutation(() => Post)
   async createPost(@Args('data') data: CreatePostInput) {
     return this.createPostUseCase.execute(data)
+  }
+
+  @Mutation(() => Post)
+  async publishPost(@Args() { id }: PostIdArgs) {
+    return this.publishPostUseCase.execute({ id })
   }
 
   @ResolveField()
